350e8cbedf710bfed5653cbf3698ef5b8c743fcd
cristiangomez27 <Suaveurban2227@gmail.com>
2026-04-13 12:30:01 -0600

    fix: agregar wrappers en /ayudantes para compatibilidad y resolución de conflictos

---
+++ ayudantes/correo.php
@@ -0,0 +1,3 @@
+<?php
+// Wrapper de compatibilidad: algunos entornos referencian /ayudantes/correo.php
+require_once dirname(__DIR__) . '/helpers/mail.php';
---
+++ ayudantes/greenapi_helper.php
@@ -0,0 +1,3 @@
+<?php
+// Wrapper de compatibilidad: algunos entornos referencian /ayudantes/greenapi_helper.php
+require_once dirname(__DIR__) . '/helpers/greenapi_helper.php';
